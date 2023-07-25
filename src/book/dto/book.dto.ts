8ba59697c95f55519bd51990f486d27c7222ecce
abdullah <abdullah_malik@hotmail.com>
2023-07-25 15:41:55 +0500

    validation through custom pipes

--- src/book/dto/book.dto.ts
+++ src/book/dto/book.dto.ts
@@ -1,7 +1,13 @@
 /* eslint-disable prettier/prettier */
 
+import { IsInt, IsString } from "class-validator";
+
 //Validation Perform
 export class BookDto{
+
+    @IsInt()
     id:number;
+    
+    @IsString()
     name:string;    
 }
--- src/book/dto/pipes/book.pipe.ts
+++ src/book/dto/pipes/book.pipe.ts
@@ -1,10 +1,28 @@
 /* eslint-disable prettier/prettier */
 import { ArgumentMetadata, BadRequestException, PipeTransform } from "@nestjs/common";
+import { plainToClass } from "class-transformer";
+import { BookDto } from "../book.dto";
+import { validate } from "class-validator";
 
 export class BookPipe implements PipeTransform{
-    transform(value: any, metadata: ArgumentMetadata) {
-        if(value.id == 1) return value
-        else throw new BadRequestException("Validation Failed.");
+    async transform(value: any, metadata: ArgumentMetadata) : Promise<any>{
+        // if(value.id == 1) return value
+        // else throw new BadRequestException("Validation Failed.");
+
+        //1. class transformer object convert to class
+        const bookClass = plainToClass(BookDto,value);
+
+        //2. validate class
+        const errors = await validate(bookClass);
+
+        if(errors.length > 0){
+            throw new BadRequestException('Validation Failed' + JSON.stringify(errors));
+        }
+
+        console.log(value, typeof(value));
+        return value;
     }
+
+    ////////////IMPORTANT/////////////// YOU CAN USE BUILT IN VALIDATION PIPE too instead of all this.///////////////////////////////
 
 }
